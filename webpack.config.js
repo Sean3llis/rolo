d53dc35bc565730c081196e5eb8f3360b9dcbf70
Sean Ellis <seane235@gmail.com>
2016-12-23 15:48:49 -0500

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
-    contentBase: './public',
+    contentBase: path.join(__dirname, 'public'),
     port: 4000,
     progress: true
   },
